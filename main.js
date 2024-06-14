a4c9ec8d835383adc93ef47fdda447de19baa124
isabelarmo <127421242+isabelarmo@users.noreply.github.com>
2024-06-14 08:55:06 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const contador = document.querySelectorAll(".contador");
+const tempoObjetivo1 = newDate (2024,6,5,12,35);
+const tempoObjetivo2 = newDate (2024,6,15,00,28);
+const tempoObjetivo3 = newDate (2024,6,30,20,35);
+const tempoObjetivo4 = newDate (2024,11,13,12,35);
 
+contador[0].textContent = "Olá";
 for (let i = 0; i < botoes.length; i++){
     botoes[i].onclick = function(){
         for (let j = 0; j < botoes.length; j++){
